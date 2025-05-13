16f98421a3b1c16f7e2e752fbfd2e2874686de5c
mailtoraushanpratap <mailtoraushanpratap@gmail.com>
2025-05-13 17:58:22 +0530

    Added the requirements.md file.

--- src/main/java/org/example/evaluations/EvaluationsApplication.java
+++ src/main/java/org/example/evaluations/EvaluationsApplication.java
@@ -3,7 +3,7 @@ package org.example.evaluations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.example.evaluations")
 public class EvaluationsApplication {
 
     public static void main(String[] args) {
